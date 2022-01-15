c6901d72b48c8803b35bd203ebcfe788cb94091f
julien <julien.boullet13@gmail.com>
2022-01-15 11:52:28 -0500

    ajout colonnes temoignage projets table

--- database/migrations/2022_01_15_152844_add_temoignages_to_projets_table.php
+++ database/migrations/2022_01_15_152844_add_temoignages_to_projets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTemoignageToProjetsTable extends Migration
+class AddTemoignagesToProjetsTable extends Migration
 {
     /**
      * Run the migrations.
