f2c28d8509c57ad6b2409cf3940a05fb7c5d3699
julien <julien.boullet13@gmail.com>
2022-01-26 12:55:58 -0500

    optimisation call to action

--- resources/views/components/call-to-action-1.blade.php
+++ resources/views/components/call-to-action-1.blade.php
@@ -13,7 +13,7 @@
                     @method('POST')
                     <div class="text-center xl:text-right md:space-x-3">
                         <label class="relative block md:inline-block w-full md:w-[30%]">
-                            <input type="text" name="nom" class="custom-input-2" placeholder="Nom">
+                            <input type="text" name="nom" class="custom-input-2" placeholder="Nom" value="{{ old('nom') }}">
                             <i class="absolute top-2 left-3 text-gray-300">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
@@ -21,7 +21,7 @@
                             </i>
                         </label>
                         <label class="relative block md:inline-block w-full md:w-[40%] mt-4 md:mt-0">
-                            <input type="email" name="email" class="custom-input-2" placeholder="Courriel">
+                            <input type="email" name="email" class="custom-input-2" placeholder="Courriel" value="{{ old('email') }}">
                             <i class="absolute top-2 left-3 text-gray-300">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
