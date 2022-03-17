d0634b9c225133d4aa23f40721bc5fbb9cafda5d
julien <julien.boullet13@gmail.com>
2022-03-17 10:56:44 -0400

    ajout CTA page projet

--- resources/views/projets/show.blade.php
+++ resources/views/projets/show.blade.php
@@ -100,17 +100,17 @@
             @endif
 
 
-{{--            <div class="flex bg-[#021223] px-14 py-4 rounded-lg text-white">--}}
-{{--                <div class="w-full md:w-2/3 flex flex-col justify-center">--}}
-{{--                    <h4 class="font-title text-3xl mb-2">Vous avez envie de <br> <span class="text-mtom-orange">booster</span> votre projet ?</h4>--}}
-{{--                    <div>--}}
-{{--                        <x-bouton-icon :lien="route('contact')">Contactez-nous</x-bouton-icon>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="hidden md:inline-flex w-1/3">--}}
-{{--                    <img src="{{ asset('storage/img/visuels/to-the-stars-2.gif') }}" class="rounded" alt="image projet-gorczany-llc-MtoM Création - Création de site web et stratégies numerique à Montréal">--}}
-{{--                </div>--}}
-{{--            </div>--}}
+            <div class="flex bg-[#021223] px-14 py-4 rounded-lg text-white">
+                <div class="w-full md:w-2/3 flex flex-col justify-center">
+                    <h4 class="font-title text-3xl mb-2">Vous avez envie de <br> <span class="text-mtom-orange">booster</span> votre projet ?</h4>
+                    <div>
+                        <x-bouton-icon :lien="route('contact')">Contactez-nous</x-bouton-icon>
+                    </div>
+                </div>
+                <div class="hidden md:inline-flex w-1/3">
+                    <img src="{{ asset('storage/img/visuels/to-the-stars-2.gif') }}" class="rounded" alt="image projet-gorczany-llc-MtoM Création - Création de site web et stratégies numerique à Montréal">
+                </div>
+            </div>
 
         </div>
 
