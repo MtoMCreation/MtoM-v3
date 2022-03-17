e51bcc53dcc12876f3352d4343ce4b9a9d1ece0b
julien <julien.boullet13@gmail.com>
2022-03-17 11:49:24 -0400

    optimisations visuelles

--- resources/views/projets/show.blade.php
+++ resources/views/projets/show.blade.php
@@ -77,7 +77,7 @@
 
         <div class="max-w-[1000px] container">
 
-            <div class="mb-20 mt-6 article-content">
+            <div class="mb-10 mt-6 article-content">
                 <div class="mt-10 text-xl leading- list-disc" style="max-width: 900px; margin: auto">
                     {!! $projet->reponse !!}
                 </div>
